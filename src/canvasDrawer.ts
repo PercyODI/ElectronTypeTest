85ebb9a2dfa877ddce33e78ac30dffd8c84e5f95
Pearse Hutson <Pearse.2008@gmail.com>
2018-01-25 22:13:54 -0600

    init

---
+++ src/canvasDrawer.ts
@@ -0,0 +1,5 @@
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
+
+import { line } from "./models/line";
+
+$('#canvas').click(() => alert("Testing!"));
---
+++ src/models/line.ts
@@ -0,0 +1,10 @@
+interface line {
+    startX: number,
+    startY: number, 
+    endX: number,
+    endY: number,
+    color: string,
+    radius: number
+}
+
+export {line};
